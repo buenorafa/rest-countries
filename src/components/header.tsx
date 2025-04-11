6cd93d4897f95933d71544930ab5339b2a4424a2
buenorafa <rafaelbuenooo@outlook.com>
2025-04-11 12:17:27 -0300

    adiciona comportamento responsivo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,9 +2,11 @@ import { ThemeToggle } from "./theme-toggle";
 
 function Header() {
   return (
-    <header className="py-4 px-16 border-b ">
-      <div className="flex justify-between items-center border-2">
-        <h1 className="ml-4 text-2xl font-extrabold">Where in the world?</h1>
+    <header className="py-4 px-4 md:px-16 shadow-md dark:bg-card">
+      <div className="flex justify-between items-center">
+        <h1 className="ml-4 text-md md:text-2xl font-extrabold">
+          Where in the world?
+        </h1>
         <nav>
           <ThemeToggle />
         </nav>
