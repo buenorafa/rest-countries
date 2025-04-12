c49ab662fff78d2bcf4e73dc46f8b8edc7a63186
buenorafa <rafaelbuenooo@outlook.com>
2025-04-12 16:40:03 -0300

    adiciona a interface de types ao cardprops

--- src/components/country-card.tsx
+++ src/components/country-card.tsx
@@ -1,21 +1,15 @@
 import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import { CountryListItem } from "@/types/country";
 import { Link } from "react-router-dom";
 
 interface CountryCardProps {
-  country: {
-    flag: string;
-    name: string;
-    population: number;
-    region: string;
-    capital?: string;
-    alpha3code: string;
-  };
+  country: CountryListItem;
 }
 
 export default function CountryCard({ country }: CountryCardProps) {
   return (
     <Link to={`/country/${country.alpha3code.toLowerCase()}`}>
-      <Card className="overflow-hidden border-none bg-card shadow hover:shadow-xl rounded min-h-96 md:max-w-66 md:min-h-84">
+      <Card className="overflow-hidden border-none bg-card shadow hover:shadow-xl rounded min-h-96 md:max-w-66 md:min-h-84 h-96">
         <img
           src={country.flag}
           alt={country.name + "flag"}
@@ -27,7 +21,9 @@ export default function CountryCard({ country }: CountryCardProps) {
         <CardContent className="space-y-1 p-6 text-sm">
           <p>
             <span className="font-semibold">Population:</span>{" "}
-            {country.population.toLocaleString()}
+            {typeof country.population === "number"
+              ? country.population.toLocaleString()
+              : "N/A"}
           </p>
           <p>
             <span className="font-semibold">Region:</span> {country.region}
