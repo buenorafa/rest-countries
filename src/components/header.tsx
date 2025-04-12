1adc8244db3963c07e1a74b80ed5287b28685b2b
buenorafa <rafaelbuenooo@outlook.com>
2025-04-12 16:41:23 -0300

    aumenta o margin bottom

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import { ThemeToggle } from "./theme-toggle";
 
 function Header() {
   return (
-    <header className="py-4 px-4 mb-8 md:px-16 shadow-md dark:bg-card">
+    <header className="py-4 px-4 mb-12 md:px-16 shadow-md dark:bg-card">
       <div className="flex justify-between items-center">
         <h1 className="ml-4 text-md md:text-2xl font-extrabold">
           Where in the world?
